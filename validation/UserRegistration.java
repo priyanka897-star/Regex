6f884fb1c528c58463285fde6efe3aec625adf07
priyanka897-star <83417119+priyanka897-star@users.noreply.github.com>
2021-10-26 00:01:24 +0530

    Refactored code

--- validation/UserRegistration.java
+++ validation/UserRegistration.java
@@ -7,33 +7,30 @@ public class UserRegistration {
 	 
 	public static  void userValidation() {
 		 Scanner scanner = new Scanner(System.in);
-		boolean check = true;
+		  boolean check = true;
 	    while (check) {
 		  System.out.println("Enter Your option to validate\n  1.FirstName\n ");
 		  int option = scanner.nextInt();
 		  scanner.nextLine();
-		 switch (option) {
+		  switch (option) {
 		  case 1:
 			final String REGEX_FIRSTNAME = "([A-Z]{1}[a-zA-Z]{3,})";
 			System.out.println("Enter First name :");
 			String firstName = scanner.next();
-		    boolean  resultForFirstName = Pattern.matches(REGEX_FIRSTNAME, firstName);
-		    System.out.println(resultForFirstName);
-		    if(resultForFirstName == false) {
-		    	System.out.println("Enter valid First Name");
+		        boolean  resultForFirstName = Pattern.matches(REGEX_FIRSTNAME, firstName);
+		        System.out.println(resultForFirstName);
+		        if(resultForFirstName == false) {
+		    	   System.out.println("Enter valid First Name");
 			break;
 		    }
-		    
 		  default:
 			 check = false;
 				break;
-				 
 		}
 	}
 }
 	public static void main(String[] args) {
 		UserRegistration.userValidation();
-		
 	}
 }	
 	
