378bb5d9cdfe83d10f8aff52944e904b70c74d7a
priyanka897-star <priyankamore8997@gmail.com>
2021-10-26 02:24:42 +0530

    Added:Functionality to validate sample email using Regex

--- validation/UserRegistration.java
+++ validation/UserRegistration.java
@@ -1,76 +1,46 @@
 package validation;
 
-import java.util.Scanner;
 import java.util.regex.*;
 
 public class UserRegistration {
 
 	public static void userValidation() {
-		Scanner scanner = new Scanner(System.in);
-		boolean check = true;
-		while (check) {
-			System.out.println(
-					"Enter Your option to validate\n  1.First Name\n  2.Last Name\n  3.Email\n  4.Mobile No\n  5.Password\n ");
-			int option = scanner.nextInt();
-			scanner.nextLine();
-			switch (option) {
-			case 1:
-				final String FIRSTNAME = "^([A-Z]{1}[a-zA-Z]{3,})";
-				System.out.println("Enter First name :");
-				String firstName = scanner.next();
-				boolean resultForFirstName = Pattern.matches(FIRSTNAME, firstName);
-				System.out.println(resultForFirstName);
-				if (resultForFirstName == false) {
-					System.out.println("Enter valid First Name");
-					break;
-				}
-			case 2:
-				final String LASTNAME = "^([A-Z]{1}[a-zA-Z]{3,})";
-				System.out.println("Enter Last name :");
-				String lastName = scanner.next();
-				boolean resultForLastName = Pattern.matches(LASTNAME, lastName);
-				System.out.println(resultForLastName);
-				if (resultForLastName == false) {
-					System.out.println("Enter valid Last Name");
-					break;
-				}
-			case 3:
-				final String EMAIL = "^([a-zA-Z0-9_.-]+)([\\\\.])([a-zA-Z]+)@([a-zA-Z]+)([\\.])([a-zA-Z]+)([\\\\.])([a-zA-Z]+)";
-				System.out.println("Enter email :");
-				String mail = scanner.nextLine();
-				boolean resultEmail = Pattern.matches(EMAIL, mail);
-				System.out.println(resultEmail);
-				if (resultEmail == false) {
-					System.out.println("Enter valid Email in abc.xyz@bl.com.in pattern");
-					break;
-				}
+		final String EMAIL = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+		boolean result1 = Pattern.matches(EMAIL, "abc@yahoo.com");
+		System.out.println("abc@yahoo.com" + ":" + result1);
 
-			case 4:
+		boolean result2 = Pattern.matches(EMAIL, "abc-100@yahoo.com");
+		System.out.println("abc-100@yahoo.com" + ":" + result2);
 
-				final String MOBILE_NUMBER = "^(\\d{2})( )([7-9]{1})(\\d{9})";
-				System.out.println("Enter Mobile Number");
-				String mobileNumber = scanner.nextLine();
-				boolean resultMobileNumber = Pattern.matches(MOBILE_NUMBER, mobileNumber);
-				System.out.println(resultMobileNumber);
-				if (resultMobileNumber == false) {
-					System.out.println("Enter valid Mobile number 91 9856238545  pattern");
-					break;
-				}
-			case 5:
-				final String PASSWORD = "^([A-Z]{1}[a-zA-Z0-9]{8,}[@#$%^&+=]{1}[0-9]{1})";
-				System.out.println("Enter Password");
-				String passWord = scanner.nextLine();
-				boolean resultPassword = Pattern.matches(PASSWORD, passWord);
-				System.out.println(resultPassword);
-				if (resultPassword == false) {
-					System.out.println("Enter valid password atleast 8 charactor and one capital latter");
-					break;
-				}
-			default:
-				check = false;
-				break;
-			}
-		}
+		boolean result3 = Pattern.matches(EMAIL, "abc.100@yahoo.com");
+		System.out.println("abc.100@yahoo.com" + ":" + result3);
+
+		boolean result4 = Pattern.matches(EMAIL, "abc111@abc.com");
+		System.out.println("abc111@abc.com" + ":" + result4);
+
+		boolean result5 = Pattern.matches(EMAIL, "abc-100@abc.net");
+		System.out.println("abc-100@abc.net" + ":" + result5);
+
+		boolean result6 = Pattern.matches(EMAIL, "abc.100@abc.com.au");
+		System.out.println("abc.100@abc.com.au" + ":" + result6);
+
+		boolean result7 = Pattern.matches(EMAIL, "abc");
+		System.out.println("abc" + ":" + result7);
+
+		boolean result8 = Pattern.matches(EMAIL, "abc@%*.com");
+		System.out.println("abc@%*.com" + ":" + result8);
+
+		boolean result9 = Pattern.matches(EMAIL, "abc()*@gmail.com");
+		System.out.println("abc()*@gmail.com" + ":" + result9);
+
+		boolean result10 = Pattern.matches(EMAIL, "abc@abc@gmail.com");
+		System.out.println("abc@abc@gmail.com" + ":" + result10);
+
+		boolean result11 = Pattern.matches(EMAIL, "abc@gmail.com.au");
+		System.out.println("abc@gmail.com.au" + ":" + result11);
+
+		boolean result12 = Pattern.matches(EMAIL, "abc()*@gmail.com");
+		System.out.println("abc()*@gmail.com" + ":" + result6);
 	}
 
 	public static void main(String[] args) {
